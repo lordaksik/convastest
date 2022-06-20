303bc2365b87b39c4b2e1201c81a7c36df899fe8
lordaksik <lordaks@tut.by>
2022-06-20 15:20:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+draw();
+function draw() {
+    const canvas = document.getElementById('tutorial');
+    const ctx = canvas.getContext('2d');
+    var fon = new Image();
+    ctx.fillRect(100,50,600,40)
+    ctx.fillRect(1200,50,600,40)
+    //1900
+}
