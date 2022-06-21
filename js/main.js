f03f436f3400fba4b28cc54831be51df9dcc432f
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-06-22 02:30:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-draw();
-function draw() {
-    const canvas = document.getElementById('tutorial');
-    const ctx = canvas.getContext('2d');
-    var fon = new Image();
-    ctx.fillRect(100,50,600,40)
-    ctx.fillRect(1200,50,600,40)
-    //1900
+
+var canvas = document.getElementById('games');
+if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+  canvas.width = 1500
+  canvas.height = 720
 }
+fonHealths()
+
+
+function fonHealths() {
+  var img = new Image();
+  img.src = 'img/1700.jpg'
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'OrangeRed';
+    ctx.strokeRect(100, 40, 550, 50)
+    ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
+    ctx.fillRect(100, 40, 550, 50);
+
+    ctx.strokeStyle = 'OrangeRed';
+    ctx.strokeRect(850, 40, 550, 50)
+    ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
+    ctx.fillRect(850, 40, 550, 50);
+  };
+}
+
+
