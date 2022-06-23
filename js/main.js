77746007b140f02cf75b4dbd08d98e55d6e880f9
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-06-24 01:54:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,24 +5,36 @@ if (canvas.getContext) {
   canvas.width = 1500
   canvas.height = 720
 }
-fonHealths()
+var img = new Image();
+img.src = 'img/1700.jpg'
+var timer = 100;
+function draw() {
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = 'OrangeRed';
+  ctx.strokeRect(100, 40, 550, 50)
+  ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
+  ctx.fillRect(100, 40, 550, 50);
 
+  ctx.strokeStyle = 'OrangeRed';
+  ctx.strokeRect(850, 40, 550, 50)
+  ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
+  ctx.fillRect(850, 40, 550, 50);
+  ctx.
+  ctx.arc(750, 40, 98, 0, Math.PI, false)
+  ctx.lineTo(652, 0)
+  ctx.lineTo(848, 0)
+  ctx.lineTo(848, 40)
+  ctx.stroke();
+  ctx.font = '50px serif';
+  ctx.fillStyle = 'black'
+  ctx.fillText(timer, 720, 80);
+  timer--
+  if (timer > 0) {
+    setTimeout((draw), 1000)
 
-function fonHealths() {
-  var img = new Image();
-  img.src = 'img/1700.jpg'
-  img.onload = function () {
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = 'OrangeRed';
-    ctx.strokeRect(100, 40, 550, 50)
-    ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
-    ctx.fillRect(100, 40, 550, 50);
-
-    ctx.strokeStyle = 'OrangeRed';
-    ctx.strokeRect(850, 40, 550, 50)
-    ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
-    ctx.fillRect(850, 40, 550, 50);
-  };
+  }
 }
+
+img.onload = draw;
 
 
