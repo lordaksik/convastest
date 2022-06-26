0498800253b8eafd74adfb6220b3e52469fac240
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-06-27 01:39:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 var canvas = document.getElementById('games');
 if (canvas.getContext) {
   var ctx = canvas.getContext('2d');
@@ -8,6 +7,64 @@ if (canvas.getContext) {
 var img = new Image();
 img.src = 'img/1700.jpg'
 var timer = 100;
+var center = 720;
+let player1 = {
+  x: 300,
+  y: 400
+}
+let player2 = {
+  x: 1100,
+  y: 400
+}
+
+document.addEventListener("keydown", playerFirst);
+document.addEventListener("keydown", playerTwo);
+
+
+function playerFirst(event) {
+  console.log(event.code)
+  if (event.code === 'KeyA') {
+
+    if (player1.x > 0) {
+      player1.x = player1.x - 20;
+    }
+  }
+
+  if (event.code === 'KeyW' && player1.y === 400) {
+
+    if (player1.y > 0) {
+      player1.y = player1.y - 200;
+    }
+  }
+  if (event.code === 'KeyD') {
+    if (player1.x < 1450) {
+      player1.x = player1.x + 20;
+    }
+  }
+}
+
+function playerTwo(event) {
+  console.log(event.code)
+  if (event.code === 'ArrowLeft') {
+
+    if (player2.x > 0) {
+      player2.x = player2.x - 20;
+    }
+  }
+  if (event.code === 'ArrowUp' && player2.y === 400) {
+
+    if (player2.y > 0) {
+      player2.y = player2.y - 200;
+    }
+  }
+   if (event.code === 'ArrowRight') {
+    if (player2.x < 1450) {
+      player2.x = player2.x + 20;
+    }
+  }
+}
+
+
 function draw() {
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
   ctx.strokeStyle = 'OrangeRed';
@@ -19,22 +76,33 @@ function draw() {
   ctx.strokeRect(850, 40, 550, 50)
   ctx.fillStyle = 'LawnGreen'; // меняем цвет клеток
   ctx.fillRect(850, 40, 550, 50);
-  ctx.
-  ctx.arc(750, 40, 98, 0, Math.PI, false)
-  ctx.lineTo(652, 0)
-  ctx.lineTo(848, 0)
-  ctx.lineTo(848, 40)
-  ctx.stroke();
+
   ctx.font = '50px serif';
   ctx.fillStyle = 'black'
-  ctx.fillText(timer, 720, 80);
-  timer--
-  if (timer > 0) {
-    setTimeout((draw), 1000)
-
+  if (timer < 10) {
+    center = 740;
   }
+  ctx.fillText(timer, center, 80);
+
+  ctx.fillStyle = 'red'; // меняем цвет клеток
+  if (player1.y < 400) {
+    player1.y = player1.y + 1;
+  }
+  ctx.fillRect(player1.x, player1.y, 50, 200);
+
+  ctx.fillStyle = 'yellow'; // меняем цвет клеток
+  if (player2.y < 400) {
+    player2.y = player2.y + 1;
+  }
+  ctx.fillRect(player2.x, player2.y, 50, 200);
+
+  requestAnimationFrame(draw)
 }
 
+function texts() {
+  if (timer > 0) {
+    timer--
+  }
+}
+setInterval(texts, 1000)
 img.onload = draw;
-
-
