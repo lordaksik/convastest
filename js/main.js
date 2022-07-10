64b78bed7789ced12d6462e680315339a6f34d78
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-07-11 01:32:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,17 @@ var img = new Image();
 img.src = 'img/1700.jpg'
 var timer = 100;
 var center = 720;
+var pressedKeys = {
+  KeyA:false,
+  KeyW:false,
+  KeyD:false,
+  KeyE:false
+};
+var pressedKeys2 = {
+  ArrowLeft:false,
+  ArrowUp:false,
+  ArrowRight:false
+};
 let player1 = {
   x: 300,
   y: 400
@@ -18,49 +29,91 @@ let player2 = {
 }
 
 document.addEventListener("keydown", playerFirst);
+document.addEventListener("keyup", playerUp);
+document.addEventListener("keyup", playerUp2);
 document.addEventListener("keydown", playerTwo);
 
-
-function playerFirst(event) {
-  console.log(event.code)
-  if (event.code === 'KeyA') {
-
+function movePlayr(){
+  if (pressedKeys.KeyA===true) {
     if (player1.x > 0) {
-      player1.x = player1.x - 20;
+      player1.x = player1.x - 2;
     }
   }
-
-  if (event.code === 'KeyW' && player1.y === 400) {
-
-    if (player1.y > 0) {
-      player1.y = player1.y - 200;
-    }
-  }
-  if (event.code === 'KeyD') {
-    if (player1.x < 1450) {
-      player1.x = player1.x + 20;
-    }
+  if (pressedKeys.KeyW===true) {
+  if (player1.y > 150) {
+    player1.y = player1.y - 10;
   }
 }
+if (pressedKeys.KeyD===true) {
+  if (player1.x < 1450) {
+    player1.x = player1.x + 2;
+  }
+}
+}
+function movePlayrTwo(){
+  if (pressedKeys2.ArrowLeft===true) {
+    if (player2.x > 0) {
+      player2.x = player2.x - 2;
+    }
+  }
+  if (pressedKeys2.ArrowUp===true) {
+  if (player2.y > 150) {
+    player2.y = player2.y - 10;
+  }
+}
+if (pressedKeys2.ArrowRight===true) {
+  if (player2.x < 1450) {
+    player2.x = player2.x + 2;
+  }
+}
+}
+function playerFirst(event) {
+  
+  if(event.code === 'KeyA') {
+      pressedKeys.KeyA = true;
+ }
+  if (event.code === 'KeyW') {
+      pressedKeys.KeyW = true;
+  }
+  if (event.code === 'KeyD') {
+    pressedKeys.KeyD = true;
+}
+}
+function playerUp(event) {
+  // console.log(event.code)
+  if(event.code === 'KeyA') {
+       pressedKeys.KeyA = false;
+     }
+     if(event.code === 'KeyW') {
+      pressedKeys.KeyW = false;
+    }
+    if(event.code === 'KeyD') {
+      pressedKeys.KeyD = false;
+    }
+    }
+    
+function playerUp2(event) {
+  if (event.code === 'ArrowLeft') {
+    pressedKeys2.ArrowLeft=false;
+  }
+  if (event.code === 'ArrowUp') {
+    pressedKeys2.ArrowUp=false;
+  }
+   if (event.code === 'ArrowRight') {
+    pressedKeys2.ArrowRight=false;
+  }
+    }
 
 function playerTwo(event) {
   console.log(event.code)
   if (event.code === 'ArrowLeft') {
-
-    if (player2.x > 0) {
-      player2.x = player2.x - 20;
-    }
+    pressedKeys2.ArrowLeft=true;
   }
-  if (event.code === 'ArrowUp' && player2.y === 400) {
-
-    if (player2.y > 0) {
-      player2.y = player2.y - 200;
-    }
+  if (event.code === 'ArrowUp') {
+    pressedKeys2.ArrowUp=true;
   }
    if (event.code === 'ArrowRight') {
-    if (player2.x < 1450) {
-      player2.x = player2.x + 20;
-    }
+    pressedKeys2.ArrowRight=true;
   }
 }
 
@@ -82,17 +135,30 @@ function draw() {
   if (timer < 10) {
     center = 740;
   }
+  movePlayr()
+  movePlayrTwo()
   ctx.fillText(timer, center, 80);
 
   ctx.fillStyle = 'red'; // меняем цвет клеток
   if (player1.y < 400) {
-    player1.y = player1.y + 1;
+    player1.y = player1.y + 3;
   }
+ 
   ctx.fillRect(player1.x, player1.y, 50, 200);
+
+  ctx.beginPath();
+  ctx.moveTo(player1.x+50,player1.y+50);
+  ctx.lineTo(player1.x+130,player1.y+20);
+  ctx.lineTo(player1.x+130,player1.y+45);
+  ctx.lineTo(player1.x+50,player1.y+75);
+  ctx.fillStyle = 'yellow';
+  ctx.fill();
+   // меняем цвет клеток
+  ctx.closePath()
 
   ctx.fillStyle = 'yellow'; // меняем цвет клеток
   if (player2.y < 400) {
-    player2.y = player2.y + 1;
+    player2.y = player2.y + 3;
   }
   ctx.fillRect(player2.x, player2.y, 50, 200);
 
