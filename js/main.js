65e291e4bec4706ce773a8915589307459e5c30e
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-07-12 17:51:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,21 +4,12 @@ if (canvas.getContext) {
   canvas.width = 1500
   canvas.height = 720
 }
+let set = new Set();
 var img = new Image();
-img.src = 'img/1700.jpg'
-var timer = 100;
+img.src = 'img/fon.png'
+var timers = 100;
 var center = 720;
-var pressedKeys = {
-  KeyA:false,
-  KeyW:false,
-  KeyD:false,
-  KeyE:false
-};
-var pressedKeys2 = {
-  ArrowLeft:false,
-  ArrowUp:false,
-  ArrowRight:false
-};
+
 let player1 = {
   x: 300,
   y: 400
@@ -34,108 +25,109 @@ let player2 = {
 
 document.addEventListener("keydown", playerFirst);
 document.addEventListener("keyup", playerUp);
-document.addEventListener("keyup", playerUp2);
 document.addEventListener("keydown", playerTwo);
+document.addEventListener("keyup", playerUp2);
 
+
+function playerFirst(event) {
+
+
+  if(event.code === 'KeyA') {
+     set.add('KeyA')
+ }
+  if (event.code === 'KeyW') {
+     set.add('KeyW')
+  }
+  if (event.code === 'KeyD') {
+   set.add('KeyD')
+}
+if (event.code === 'KeyE') {
+ set.add('KeyE')
+}
+}
+function playerTwo(event) {
+
+  if (event.code === 'KeyJ') {
+    set.add('KeyJ')
+  }
+  if (event.code === 'KeyI') {
+    set.add('KeyI')
+  }
+  if (event.code === 'KeyL') {
+    set.add('KeyL')
+  }
+}
+function playerUp(event) {
+  if(event.code === 'KeyA') {
+      set.delete('KeyA')
+     }
+     if(event.code === 'KeyW') {
+  set.delete('KeyW')
+    }
+    if(event.code === 'KeyD') {
+       set.delete('KeyD')
+    }
+    if(event.code === 'KeyE') {
+       set.delete('KeyE')
+    }
+    }
+function playerUp2(event) {
+  if (event.code === 'KeyJ') {
+    set.delete('KeyJ');
+  }
+  if (event.code === 'KeyI') {
+    set.delete('KeyI');
+  }
+   if (event.code === 'KeyL') {
+     set.delete('KeyL');
+  }
+    }
 function movePlayr(){
-  if (pressedKeys.KeyA===true) {
+  if (set.has('KeyA')===true) {
     if (player1.x > 0) {
       player1.x = player1.x - 2;
     }
   }
-  if (pressedKeys.KeyW===true) {
-  if (player1.y > 150) {
-    player1.y = player1.y - 10;
+  if (set.has('KeyW')===true) {
+    if (player1.y > 150) {
+      player1.y = player1.y - 10;
+    }
   }
-}
-if (pressedKeys.KeyD===true) {
-  if (player1.x < 1450) {
-    player1.x = player1.x + 2;
+  if (set.has('KeyD')===true) {
+    if (player1.x < 1450) {
+      player1.x = player1.x + 2;
+    }
   }
-}
-if (pressedKeys.KeyE===true) {
-if (player1Atack.y1 < 115 ) {
-    player1Atack.y1 = player1Atack.y1 + 5;
-    player1Atack.y2 = player1Atack.y2 + 5;
+  if (set.has('KeyE')===true) {
+    if (player1Atack.y1 < 115 ) {
+      player1Atack.y1 = player1Atack.y1 + 5;
+      player1Atack.y2 = player1Atack.y2 + 5;
+    }
+    else{
+      player1Atack.y1=20;
+      player1Atack.y2=45;
+    }
   }
-  else{
-    player1Atack.y1=20;
-    player1Atack.y2=45;
-  }
-}
 }
 function movePlayrTwo(){
-  if (pressedKeys2.ArrowLeft===true) {
+  if (set.has('KeyJ')===true) {
     if (player2.x > 0) {
       player2.x = player2.x - 2;
     }
   }
-  if (pressedKeys2.ArrowUp===true) {
-  if (player2.y > 150) {
-    player2.y = player2.y - 10;
-  }
-}
-if (pressedKeys2.ArrowRight===true) {
-  if (player2.x < 1450) {
-    player2.x = player2.x + 2;
-  }
-}
-}
-function playerFirst(event) {
-  
-  if(event.code === 'KeyA') {
-      pressedKeys.KeyA = true;
- }
-  if (event.code === 'KeyW') {
-      pressedKeys.KeyW = true;
-  }
-  if (event.code === 'KeyD') {
-    pressedKeys.KeyD = true;
-}
-if (event.code === 'KeyE') {
-  pressedKeys.KeyE = true;
-}
-}
-function playerUp(event) {
-  // console.log(event.code)
-  if(event.code === 'KeyA') {
-       pressedKeys.KeyA = false;
-     }
-     if(event.code === 'KeyW') {
-      pressedKeys.KeyW = false;
+  if (set.has('KeyI')===true) {
+    if (player2.y > 150) {
+      player2.y = player2.y - 10;
     }
-    if(event.code === 'KeyD') {
-      pressedKeys.KeyD = false;
-    }
-    if(event.code === 'KeyE') {
-      pressedKeys.KeyE = false;
-    }
-    }
-    
-function playerUp2(event) {
-  if (event.code === 'ArrowLeft') {
-    pressedKeys2.ArrowLeft=false;
   }
-  if (event.code === 'ArrowUp') {
-    pressedKeys2.ArrowUp=false;
-  }
-   if (event.code === 'ArrowRight') {
-    pressedKeys2.ArrowRight=false;
-  }
+  if (set.has('KeyL')===true) {
+    if (player2.x < 1450) {
+      player2.x = player2.x + 2;
     }
+  }
+}
 
-function playerTwo(event) {
-  console.log(event.code)
-  if (event.code === 'ArrowLeft') {
-    pressedKeys2.ArrowLeft=true;
-  }
-  if (event.code === 'ArrowUp') {
-    pressedKeys2.ArrowUp=true;
-  }
-   if (event.code === 'ArrowRight') {
-    pressedKeys2.ArrowRight=true;
-  }
-}
+
 
 
 function draw() {
@@ -152,18 +144,18 @@ function draw() {
 
   ctx.font = '50px serif';
   ctx.fillStyle = 'black'
-  if (timer < 10) {
+  if (timers < 10) {
     center = 740;
   }
   movePlayr()
   movePlayrTwo()
-  ctx.fillText(timer, center, 80);
+  ctx.fillText(timers, center, 80);
 
   ctx.fillStyle = 'red'; // меняем цвет клеток
   if (player1.y < 400) {
     player1.y = player1.y + 3;
   }
- 
+
   ctx.fillRect(player1.x, player1.y, 50, 200);
 
   ctx.beginPath();
@@ -181,13 +173,13 @@ function draw() {
     player2.y = player2.y + 3;
   }
   ctx.fillRect(player2.x, player2.y, 50, 200);
-
+console.log(set)
   requestAnimationFrame(draw)
 }
 
 function texts() {
-  if (timer > 0) {
-    timer--
+  if (timers > 0) {
+    timers--
   }
 }
 setInterval(texts, 1000)
